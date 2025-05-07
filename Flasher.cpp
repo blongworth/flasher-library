ae81a3d3880709a8b8b2c5bb14775a81fabfd17e
Brett Longworth <blongworth@whoi.edu>
2025-05-07 15:23:26 -0400

    keep led off if onTime == 0

--- Flasher.cpp
+++ Flasher.cpp
@@ -17,42 +17,47 @@
  * BSD license, all text above must be included in any redistribution
  */
 
-#include <Arduino.h>
 #include "Flasher.h"
+#include <Arduino.h>
 
 Flasher::Flasher(int ledPin, int onTime, int offTime) {
-    _ledPin = ledPin;
-    _onTime = onTime;
-    _offTime = offTime;
-    
-    int ledState;
-    unsigned long prevMillis;
+  _ledPin = ledPin;
+  _onTime = onTime;
+  _offTime = offTime;
+
+  int ledState;
+  unsigned long prevMillis;
 }
 
 void Flasher::begin() {
-    pinMode(_ledPin, OUTPUT);
-    ledState = LOW;
-    prevMillis = 0;
+  pinMode(_ledPin, OUTPUT);
+  ledState = LOW;
+  prevMillis = 0;
 }
 
 void Flasher::run() {
-    unsigned long currMillis = millis();
+  if (_onTime == 0) {
+    if (ledState == HIGH) {
+      ledState = LOW;
+      digitalWrite(_ledPin, LOW);
+    }
+    return;
+  }
 
-    if (ledState == HIGH && currMillis - prevMillis >= _onTime)
-    {
-        ledState = LOW;
-        prevMillis = currMillis;
-        digitalWrite(_ledPin, ledState);
-    }
-    else if (ledState == LOW && currMillis - prevMillis >= _offTime)
-    {
-        ledState = HIGH;
-        prevMillis = currMillis;
-        digitalWrite(_ledPin, ledState);
-    }
+  unsigned long currMillis = millis();
+
+  if (ledState == HIGH && currMillis - prevMillis >= _onTime) {
+    ledState = LOW;
+    prevMillis = currMillis;
+    digitalWrite(_ledPin, ledState);
+  } else if (ledState == LOW && currMillis - prevMillis >= _offTime) {
+    ledState = HIGH;
+    prevMillis = currMillis;
+    digitalWrite(_ledPin, ledState);
+  }
 }
 
 void Flasher::update(int onTime, int offTime) {
-    _onTime = onTime;
-    _offTime = offTime;
+  _onTime = onTime;
+  _offTime = offTime;
 }
